58d3037837c2e3fef24cbfb3b32a009bd0787968
Bohdan Lukianchenko <bohdan.lukianchenko@sysgears.com>
2024-05-15 20:23:14 +0300

    Added global decorator

--- server/src/modules/service/modules/app-config/app-config.module.ts
+++ server/src/modules/service/modules/app-config/app-config.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { AppConfigService } from './services/app-config/app-config.service'
 
+@Global()
 @Module({
     imports: [
         ConfigModule.forRoot({
